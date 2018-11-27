b392232dc908bac6b6aa7fc4bd4cbe4c5a38e367
Company Key <lxd3808@163.com>
2018-11-27 10:34:53 +0800

    Initial commit

---
+++ src/main/java/com/yuudati/securitydemo/SecurityDemoApplication.java
@@ -0,0 +1,17 @@
+package com.yuudati.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @Auther lxd3808@gmail.com
+ * @Date 2018/11/27
+ */
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+}
